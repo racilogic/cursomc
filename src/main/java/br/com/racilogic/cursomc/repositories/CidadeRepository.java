b6775343c117f7b62f89fc680ee5a74898a0d80c
Racilogic <racilogic@gmail.com>
2018-11-13 10:41:28 -0200

    Estado e Cidade

---
+++ src/main/java/br/com/racilogic/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package br.com.racilogic.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.racilogic.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
